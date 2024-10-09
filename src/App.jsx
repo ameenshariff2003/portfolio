a0f1cff82362cfca43e5725068eb5184266ef6fd
Ameen shariff <ameenshariff0319@gmail.com>
2024-10-09 15:43:02 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // import './App.css'
 import './app.scss'
 import Navbar from './components/navbar/Navbar'
-import Hero from './components/hero/hero'
+import Hero from './components/hero/Hero'
 import Paralax from './components/paralax/Paralax'
 import Skills from './components/skills/Skills'
 import Project from './components/projects/Project'
